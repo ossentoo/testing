0b3d9aa714c051181ba788363c76ba70b92302e7
Oscar Ssentoogo <ossentoogo@nephilaadvisors.co.uk>
2020-07-27 16:00:07 +0100

    new files

--- maths/Calculator.cs
+++ maths/Calculator.cs
@@ -6,6 +6,10 @@ namespace maths
     {
         public int Add (int number1, int number2)
         {
+
+            if(number1 == 0 || number2 == 0)
+                throw new ArgumentNullException("unexpected value");
+
             var result = number1 + number2;
 
             return result;
--- mathstests/CalculatorTests.cs
+++ mathstests/CalculatorTests.cs
@@ -18,13 +18,36 @@ namespace mathstests
         }
 
         [Fact]
-        public void CalculatorTestOneAndTwoEqualsThree()
+        public void CalculatorTestTwoAndThreeEqualsFive()
         {
             var adder = new Calculator();
 
-            var result = adder.Add(1, 2);
+            var result = adder.Add(2, 3);
 
-            Assert.Equal(3, result);
-        }        
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void CalculatorTestThreeAndFourEqualsSeven()
+        {
+            var adder = new Calculator();
+
+            var result = adder.Add(3, 4);
+
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void CalculatorTestInvalidValues()
+        {
+            var adder = new Calculator();
+
+            string value = "A";
+
+            int.TryParse(value, out int t);
+            var result = adder.Add(t, 0);
+
+            Assert.Throws<ArgumentException>(() => "unexpected value");
+        }
     }
 }
