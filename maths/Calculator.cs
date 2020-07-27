162f740e9b30aa03f337dc42dad8f06fff67651c
ossentoo <ossentoo@gmail.com>
2020-07-27 15:42:42 +0100

    initialfiles

---
+++ maths/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace maths
+{
+    public class Calculator
+    {
+        public int Add (int number1, int number2)
+        {
+            var result = number1 + number2;
+
+            return result;
+        }
+    }
+}
---
+++ mathstests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using maths;
+using Xunit;
+
+namespace mathstests
+{
+    
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CalculatorTestOneAndTwoEqualsThree()
+        {
+            var adder = new Calculator();
+
+            var result = adder.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void CalculatorTestOneAndTwoEqualsThree()
+        {
+            var adder = new Calculator();
+
+            var result = adder.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }        
+    }
+}
